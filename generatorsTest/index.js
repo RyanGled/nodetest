110410cf15a025168c857e6ab03807b9bb69a0bd
adamdry <adamdryit@gmail.com>
2015-06-10 08:40:27 +0100

    .

---
+++ generatorsTest/index.js
@@ -0,0 +1,18 @@
+var mach = require('mach');
+var app  = mach.stack();
+var Q    = require('q');
+
+function sleep(millis) {
+  var deferredResult = Q.defer();
+  setTimeout(function() {
+    deferredResult.resolve();
+  }, millis);
+  return deferredResult.promise;
+};
+
+app.run(Q.async(function* (request) {
+  yield sleep(2000);
+  return 'Good day to you sir';
+}));
+
+mach.serve(app, 3333);
